0f5b96c046a93baae5a4aea7228ee93951dcdfa9
Lucas Santana <ln.santana08@gmail.com>
2022-11-23 14:21:27 -0300

    fix: wisewallet api url

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -2,5 +2,5 @@ import axios, { AxiosInstance } from 'axios';
 
 export const httpClient: AxiosInstance = axios.create({
 	baseURL:
-		process.env.NODE_ENV === 'production' ? 'teste' : 'http://localhost:3033'
+		process.env.NODE_ENV === 'production' ? 'https://wisewallet-api-production.up.railway.app/' : 'http://localhost:3033'
 });
