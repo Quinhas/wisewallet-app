b764cedc0d8506c1927df6120203b5025779ab78
Lucas Santana <ln.santana08@gmail.com>
2022-11-26 17:18:15 -0300

    feat: implementing PWA

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,8 +1,11 @@
 import axios, { AxiosInstance } from 'axios';
 
+const apiLocal = false;
+const prodUrl = apiLocal ? 'http://localhost:3033' : 'https://wisewallet-api-develop.up.railway.app/'
+
 export const httpClient: AxiosInstance = axios.create({
 	baseURL:
 		process.env.NODE_ENV === 'production'
 			? 'https://wisewallet-api-production.up.railway.app/'
-			: 'http://localhost:3033'
+			: prodUrl
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,41 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [tsconfigPaths(), react(), eslint()],
+	plugins: [
+		tsconfigPaths(),
+		react(),
+		eslint(),
+		VitePWA({
+			manifest: {
+				name: "Wisewallet",
+				short_name: "Wisewallet",
+				start_url: '/',
+				display: "standalone",
+				background_color: "#fff",
+				lang: "en",
+				scope: "/",
+				icons: [
+					{
+						src: '/android-chrome-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+					{
+						src: '/android-chrome-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+				theme_color: '#7869E2',
+			},
+			registerType: 'autoUpdate'
+		})
+	],
 })
